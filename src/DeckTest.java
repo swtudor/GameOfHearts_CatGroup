4710afdc2463ca53a0a28def2ecb3f18e7920d96
dynisha <dynishalewis25@gmail.com>
2021-07-22 09:26:40 -0500

    Merge remote-tracking branch 'origin/西瓜🍉' into Dy'Nisha
    # Conflicts:
    #	src/Deck.java
    #	src/Main.java
    #	src/Player.java

--- src/DeckTest.java
+++
@@ -1,8 +0,0 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-class DeckTest {
-    
-    @org.junit.jupiter.api.Test
-    void shuffle() {
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,20 @@
-import java.util.*;
-
+import java.util.Scanner;
 
 public class Main {
+    //Main room add the roots
+    //  scanner System
     public static void main(String[] args) {
-        System.out.println("Welcome to Heart's Card game!");
-        System.out.println("This game requires a bit more thinking.");
-        System.out.println("You and 3 other players try to trick each other!");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many players do you have?");
+        String playerString = scan.nextLine();
+        int players = Integer.parseInt(playerString);
+        if (players < 4) {
+            //  insert exit code
+            System.out.println("Sorry you must Have at 4 players to play this game \n");
+//            System.exit(0);
+        } else {
+            System.out.println("Hello and Welcome to the game of Hearts!");
+        }
+        Players.greetPlayers();
     }
 }
